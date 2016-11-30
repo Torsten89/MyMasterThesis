9dadd48ac75db2ce3156b54cc2ab6440b6710807
Torsten89 <Torsten.Knauf@gmx.de>
2016-11-30 14:31:23 +0100

    added first TreeTagger example

---
+++ SourceCode/FirstTreeTagger/Tagging.py
@@ -0,0 +1,13 @@
+#Environment variable TREETAGGER_HOME=/path/to/TreeTagger/cmd has to be set for TreeTaggers nltk
+from treetagger import TreeTagger
+
+if __name__ == '__main__':
+    tt_ger = TreeTagger(language='german')
+    # print(tt_ger.tag('Das Haus hat einen großen hübschen Garten.'))
+    tags =tt_ger.tag('Eine Stunde später gießt man die Brühe durch ein Haarsieb, weil sie nie \
+            ganz klar ist, spült das Stück Fleisch eben ab und setzt es mit der Brühe, die man \
+            vom Bodensatz langsam abschüttet, in dem ebenfalls umgespülten Topfe wieder zu Feuer \
+            nebst einigen Scorzoner-, einer Sellerie- und Petersilienwurzel')
+    
+    for t in tags:
+        print(t)
