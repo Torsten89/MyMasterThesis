af1b8e9a108be6cbb7864975089ef059b767ffb1
Torsten89 <Torsten.Knauf@gmx.de>
2017-03-13 15:17:04 +0100

    replaced overflow:visible hack of timeline, which did not work for chrome

--- docs/js/timeline.js
+++ docs/js/timeline.js
@@ -2,26 +2,25 @@ var svg = $("#mySvg");
 var svgPosi;
 var svgTop;
 var svgLeft;
+var fstLines = $("[fstLineToggle]");
+var activeContent = "";
 
 $(window).on('resize', function(){
     drawLines();
 });
 
 // expand all for test purpose
-/*$("[fstLineToggle]").each(function() {
-    var lineToToggle = $(this).attr("fstLineToggle");
-    $("#"+lineToToggle).toggle(0);
-});*/
+// $("[fstLineToggle]").each(function() { $("#"+$(this).attr("fstLineToggle")).toggle(0); });
 drawLines();
 
-//set onclick handler for fst line (expand / collapse and redraw)
+//set onclick handler for fst line (expand/collapse and redraw)
 $("[fstLineToggle]").each(function() {
     var lineToToggle = $(this).attr("fstLineToggle");
     if(lineToToggle == "") return; // last timeline item is not expandable
     
     var toggledLine = $(this);
     $(this).click(function() {
-        svg.empty(); // otherwise sndLine slides in while lines are still at their old position - which is ugly
+        svg.empty(); // otherwise sndLine slides out while lines are still at their old position - which is ugly
         $("#"+lineToToggle).toggle(400, function() {
             toggledLine.toggleClass("active");
             drawLines();
@@ -30,14 +29,13 @@ $("[fstLineToggle]").each(function() {
 });
 
 
-// deactivate old active/in and collapse content when sndLine item clicked
-var activeContent = "";
+// deactivate old in (active) and collapse content when sndLine item clicked
 $("a.timelineItem").each(function() {
     $(this).click(function () {
-        if(activeContent!= "") {
+        if(activeContent !=  "") {
             $("a[href='"+activeContent+"']").attr("aria-expanded", "false");
             $("a.timelineItem").each(function() {
-                $("#"+$(this).attr("href").replace("#", "")).removeClass("in");
+                $("#" + $(this).attr("href").replace("#", "") ).removeClass("in");
             });
         }
         activeContent = $(this).attr("href");
@@ -48,18 +46,17 @@ $("a.timelineItem").each(function() {
 function drawLines() {
     svg.empty();
     svgPosi = svg.offset();
-    svgTop = svgPosi.top; // add scroll because svg has position:absoulte
+    svgTop = svgPosi.top;
     svgLeft = svgPosi.left;
     
-    var fstLines = $("[fstLineToggle]");
     for(var i=0; i<fstLines.length-1; i++) {
         var fstLine = $(fstLines[i]);
         if(fstLine.hasClass("active")) {
             var lineToToggle = fstLine.attr("fstLineToggle");
-            if (lineToToggle == "") continue;
+            if (lineToToggle == "") continue; // last item has no expandable -> there are no sndLines to draw
             
             var sndLine = $("#"+lineToToggle).children("a");
-            connectLines(fstLine, sndLine);
+            connectLines(fstLine, sndLine[0]);
             connectMarkers(sndLine);
             connectLines(sndLine[sndLine.length-1], fstLines[i+1]);
         } else {
@@ -68,9 +65,9 @@ function drawLines() {
     }
 }
 
-function connectLines(fstL, sndL) {
-    var m1 = getCenterPoint(fstL);
-    var m2 = getCenterPoint(sndL);
+function connectLines(elem1, elem2) {
+    var m1 = getCenterPoint(elem1);
+    var m2 = getCenterPoint(elem2);
     svg.append(getLine(m1.x, m1.y, m2.x, m2.y)); 
 }
 
@@ -90,7 +87,8 @@ function getCenterPoint(elem) {
     return {x:xPosi, y:yPosi};
 }
 
-//parameters are absolute pixel values of screen (from jquerys offset)
+// parameters are absolute pixel values of screen (from jquerys offset)
+// for correct resizing of the svg y2 has to be the lower/bigger-value than y1
 function getLine(x1, y1, x2, y2) {
     var l = document.createElementNS('http://www.w3.org/2000/svg', 'line');
     l.setAttribute('x1', x1-svgLeft);
