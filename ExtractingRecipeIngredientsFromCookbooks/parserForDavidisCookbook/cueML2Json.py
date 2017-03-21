b60c5188a7b79fdbe2e3d9f442f946782b33c034
Torsten89 <Torsten.Knauf@gmx.de>
2017-03-21 11:43:37 +0100

    Removed dot from type - this is important for bootstrap id management of elems!

--- ExtractingRecipeIngredientsFromCookbooks/parserForDavidisCookbook/cueML2Json.py
+++ ExtractingRecipeIngredientsFromCookbooks/parserForDavidisCookbook/cueML2Json.py
@@ -4,7 +4,7 @@ from xml.dom.minidom import parse, parseString
 import json
 from parserForDavidisCookbook.xmlHelper import getElems
 
-def getJsonRcps(pathToCueMLFile, rcpIds, hack=True):
+def getJsonRcps(pathToCueMLFile, rcpIds, hack=False):
     dictRcps = dict()
     
     xmlRcps = (XmlParser(parse(pathToCueMLFile))).getXmlRecipes(rcpIds)
@@ -17,7 +17,7 @@ def getJsonRcps(pathToCueMLFile, rcpIds, hack=True):
     # hack for using our manual tagged soups:
     if hack:
         pathToGoldenStandard = "/home/torsten/Desktop/MyMasterThesis/DavidisKochbuch/recipes extracted.xml"
-        xmlRcps = (XmlParser(parse(pathToGoldenStandard))).getXmlRecipes(["B-{}".format(i) for i in range(1, 94)])
+        xmlRcps = (XmlParser(parse(pathToGoldenStandard))).getXmlRecipes(["B-{}".format(i) for i in range(1, 69)])
         soups = [xmlRcp2dictRcp(xmlRcp) for xmlRcp in xmlRcps]
         dictRcps["Suppen"] = soups
 
@@ -28,7 +28,7 @@ def xmlRcp2dictRcp(xmlRcp):
     dictRcp = dict()
     
     dictRcp["rcp-id"] = plainTextRcp.rcpId
-    dictRcp["type"] = plainTextRcp.rcpType.replace("_", " ") #[:-1]  # remove . at the end
+    dictRcp["type"] = plainTextRcp.rcpType.replace("_", " ")[:-1]  # remove . at the end - important for bootstrap valid id!!
     dictRcp["name"] = plainTextRcp.name
     dictRcp["instructions"] = getInstructionText(xmlRcp, "p") + getInstructionText(xmlRcp, "note")
     dictRcp["ingredients"] = mergeIngs(getIngsFromNode(xmlRcp))
@@ -66,10 +66,11 @@ def mergeIngs(ings):
                     
                     
 if __name__ == '__main__':
-    pathToCueMLFile = "/home/torsten/Desktop/erg.xml"
+    #pathToCueMLFile = "/home/torsten/Desktop/erg.xml"
+    pathToCueMLFile = "/home/torsten/Desktop/MyMasterThesis/docs/DavidisesKochbuch/Rezepte mit cueML.xml"
     pathToErg = "/home/torsten/Desktop/MyMasterThesis/docs/Rezepte/Rezepte.json"
     with open(pathToErg, "w") as f:
-        f.write("rcps={}".format(getJsonRcps(pathToCueMLFile, []))) 
+        f.write("rcps={}".format(getJsonRcps(pathToCueMLFile, [], hack=True))) 
 
     
     
